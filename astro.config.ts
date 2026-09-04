eaef419b70c05b4ec0c097200a17af426fbb32b5
Haitai Hu <hityhoo@gmail.com>
2026-09-04 23:48:20 +0800

    Remove syntax highlight

--- astro.config.ts
+++ astro.config.ts
@@ -25,10 +25,7 @@ export default defineConfig({
   },
   markdown: {
     shikiConfig: {
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
+      theme: 'css-variables',
       wrap: false
     },
     remarkPlugins: [
