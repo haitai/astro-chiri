0c7204d93291c3b514521a7d131a8a5e60d06fc1
Haitai Hu <hityhoo@gmail.com>
2026-09-04 23:41:55 +0800

    Update syntax highlight config and about content

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,10 @@ export default defineConfig({
   },
   markdown: {
     shikiConfig: {
-      theme: 'css-variables',
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
       wrap: false
     },
     remarkPlugins: [
